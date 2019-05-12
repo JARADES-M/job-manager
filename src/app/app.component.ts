a6b9e91dfbb6aa083736af939a8296d31b07a8b4
Jarades Monteiro <jaradesm@gmail.com>
2019-05-12 19:28:28 -0300

    first layout basic schema

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { inspectNativeElement } from '@angular/platform-browser/src/dom/debug/ng_probe';
 
 @Component({
   selector: 'app-root',
@@ -10,39 +11,51 @@ export class AppComponent {
   color = 'accent';
   disabled = false;
 
-  ativos: Servico[] = [
+  servicos: Servico[] = [
     {
       name: 'Serviço A',
       updated: new Date('1/1/16'),
-      checked: true
+      checked: true,
+      ativo: true
     },
     {
       name: 'Serviço B',
       updated: new Date('1/17/16'),
-      checked: true
+      checked: true,
+      ativo: true
     },
     {
       name: 'Serviço X',
       updated: new Date('1/28/16'),
-      checked: true
-    }
-  ];
-  inativos: Servico[] = [
+      checked: true,
+      ativo: true
+    },
     {
       name: 'Serviço Y',
       updated: new Date('2/20/16'),
-      checked: false
+      checked: false,
+      ativo: false
     },
     {
       name: 'Serviço Z',
       updated: new Date('1/18/16'),
-      checked: false
+      checked: false,
+      ativo: false
     }
   ];
+
+  ativarTodos() {
+    this.servicos.forEach(s => s.checked = true);
+  }
+  desativarTodos() {
+    this.servicos.forEach(s => s.checked = false);
+  }
+
 }
 
 export interface Servico {
   name: string;
   updated: Date;
-  checked: boolean
+  checked: boolean;
+  ativo: boolean;
 }

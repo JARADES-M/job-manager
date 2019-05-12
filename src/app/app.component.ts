cc2ade77fe21f24ad524e5273b121dc90b578c3d
Jarades Monteiro <jaradesm@gmail.com>
2019-05-12 17:24:28 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'job-scheduler';
+
+  color = 'accent';
+  disabled = false;
+
+  ativos: Servico[] = [
+    {
+      name: 'Serviço A',
+      updated: new Date('1/1/16'),
+      checked: true
+    },
+    {
+      name: 'Serviço B',
+      updated: new Date('1/17/16'),
+      checked: true
+    },
+    {
+      name: 'Serviço X',
+      updated: new Date('1/28/16'),
+      checked: true
+    }
+  ];
+  inativos: Servico[] = [
+    {
+      name: 'Serviço Y',
+      updated: new Date('2/20/16'),
+      checked: false
+    },
+    {
+      name: 'Serviço Z',
+      updated: new Date('1/18/16'),
+      checked: false
+    }
+  ];
+}
+
+export interface Servico {
+  name: string;
+  updated: Date;
+  checked: boolean
 }

b20e58c3e6cbf8836b2854377a0e1a16387168fd
Jarades Monteiro <jaradesm@gmail.com>
2019-05-12 19:56:18 -0300

    update interface functionalities, remove duplicates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,39 +16,37 @@ export class AppComponent {
       name: 'Serviço A',
       updated: new Date('1/1/16'),
       checked: true,
-      ativo: true
     },
     {
       name: 'Serviço B',
       updated: new Date('1/17/16'),
       checked: true,
-      ativo: true
     },
     {
       name: 'Serviço X',
       updated: new Date('1/28/16'),
       checked: true,
-      ativo: true
     },
     {
       name: 'Serviço Y',
       updated: new Date('2/20/16'),
       checked: false,
-      ativo: false
     },
     {
       name: 'Serviço Z',
       updated: new Date('1/18/16'),
       checked: false,
-      ativo: false
     }
   ];
 
   ativarTodos() {
-    this.servicos.forEach(s => s.checked = true);
+    this.servicos.map(s => s.checked = true);
   }
   desativarTodos() {
-    this.servicos.forEach(s => s.checked = false);
+    this.servicos.map(s => s.checked = false);
+  }
+  alterarStatus(servico: Servico){
+    this.servicos.filter(s => s.name == servico.name).map(s => s.checked = !servico.checked);
   }
 
 }
@@ -57,5 +55,4 @@ export interface Servico {
   name: string;
   updated: Date;
   checked: boolean;
-  ativo: boolean;
 }
